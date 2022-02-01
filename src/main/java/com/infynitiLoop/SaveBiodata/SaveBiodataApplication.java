78b649c11a50cd40bdefc694f7791591c9e36967
Neha <er.neha.singh@gmail.com>
2022-02-01 12:52:44 +1100

    initial commit

---
+++ src/main/java/com/infynitiLoop/SaveBiodata/SaveBiodataApplication.java
@@ -0,0 +1,14 @@
+package com.infynitiLoop.SaveBiodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveBiodataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveBiodataApplication.class, args);
+		System.out.print("holaaa");
+	}
+
+}
